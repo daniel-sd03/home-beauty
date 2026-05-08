bcf07db91d7b0ec52a2054a60519fd182e3ee171
Daniel Sodré <dsodre2003@gmail.com>
2026-05-08 19:15:52 -0300

    fix(security): allow public access to GET /categories (#37)

--- src/main/java/sodresoftwares/homebeauty/infra/security/SecurityConfigurations.java
+++ src/main/java/sodresoftwares/homebeauty/infra/security/SecurityConfigurations.java
@@ -35,6 +35,7 @@ public class SecurityConfigurations {
 						.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
 						.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
 						.requestMatchers(HttpMethod.POST, "/auth/register/professional").permitAll()
+						.requestMatchers(HttpMethod.GET, "/categories").permitAll()
 						.requestMatchers("/error").permitAll()
 						.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
 						.requestMatchers(HttpMethod.PATCH, "/auth/{id}/role/admin").hasRole("ADMIN")
